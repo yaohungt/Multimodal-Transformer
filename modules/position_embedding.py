e2259032d5c018a35c7898039dad73e755870976
Nid989 <75028682+Nid989@users.noreply.github.com>
2022-09-12 10:40:02 +0530

    debugging changes #6

--- modules/position_embedding.py
+++ modules/position_embedding.py
@@ -73,7 +73,7 @@ class SinusoidalPositionalEmbedding(nn.Module):
             )
         self.weights[device] = self.weights[device].type_as(self._float_tensor)
         positions = make_positions(input, self.padding_idx, self.left_pad)
-        return self.weights[device].index_select(0, positions.view(-1)).view(bsz, seq_len, -1).detach()
+        return self.weights[device].index_select(0, positions.reshape(-1)).reshape(bsz, seq_len, -1).detach()
 
     def max_positions(self):
         """Maximum number of supported positions."""
